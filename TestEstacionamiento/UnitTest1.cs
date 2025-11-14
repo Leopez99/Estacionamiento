51ed34ed808496a9476bc5c91aabe3344219766e
leopez99 <leonardo@pezoa.com>
2025-11-14 20:30:56 -0300

    Cambios en la forma que esta organizada la solucion

--- TestEstacionamiento/UnitTest1.cs
+++ TestEstacionamiento/UnitTest1.cs
@@ -6,16 +6,17 @@ namespace TestEstacionamiento
     public class UnitTest1
     {
         Vehiculo camioneta;
+        Vehiculo auto;
         CreadorDeVehiculo creadorDeVehiculo;
         public UnitTest1()
         {
             creadorDeVehiculo = new CreadorDeVehiculo();
         }
 
-        [Fact]
+        [Fact]  //No funciona porque el metodo crearVehiculo hace cosas con readLine() y necesita la consola
         public void CreacionDeCamioneta()
         {
-            camioneta = creadorDeVehiculo.crearVehiculo(1);
+            camioneta = creadorDeVehiculo.crearVehiculo(1); 
             Assert.Equal("C123", camioneta.matricula);
         }
     }
