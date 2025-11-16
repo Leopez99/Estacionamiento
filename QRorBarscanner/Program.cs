e352ab24fe30d41a1825ac9b75e1189eabc7200d
leopez99 <leonardo@pezoa.com>
2025-11-16 20:24:52 -0300

    Agrego un creador de QR

---
+++ QRorBarscanner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+namespace QRorBarscanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The payload aka the text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            qrCodeImage.Save("QR.png", ImageFormat.Png);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
