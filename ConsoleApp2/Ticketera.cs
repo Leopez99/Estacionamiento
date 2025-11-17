d788541c427ee780c6f253ad84e67ad33bffc07f
leopez99 <leonardo@pezoa.com>
2025-11-17 14:14:16 -0300

    Creacion de ticketera

---
+++ ConsoleApp2/Ticketera.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Ticketera
+    {
+        //La ticketera entrega un qr que tiene la informacion de la hora de entrada del auto
+        //y una foto con su patente, tambien debe identificar si es una moto, camioneta o auto
+
+        public void CrearTicket()
+        {
+
+        }
+
+    }
+}
